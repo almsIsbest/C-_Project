740315c9bfa97244d9417cb1a17560dfbeadc3e7
almslsbest <185805234@qq.com>
2024-10-16 17:59:43 +0800

    Update .gitignore and remove cmake-build-debug from version control

--- cmake-build-debug/CMakeFiles/preprocess_chapter_demo1.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/preprocess_chapter_demo1"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/preprocess_chapter_demo1.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
--- cmake-build-debug/CMakeFiles/preprocess_chapter_demo1.dir/compiler_depend.make
+++
@@ -1,2 +0,0 @@
-# Empty custom commands generated dependencies file for preprocess_chapter_demo1.
-# This may be replaced when dependencies are built.
